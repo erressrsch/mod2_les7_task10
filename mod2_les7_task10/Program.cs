8a7a3efd3d34f7de9021c2594d8aeb7840d313ca
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:20:16 +0100

    Add project files.

---
+++ mod2_les7_task10/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace mod2_les7_task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sideLenghts = [];
+
+            Console.WriteLine("Enter side A: ");
+            int sideA = int.Parse(Console.ReadLine());
+            sideLenghts.Add(sideA);
+
+            Console.WriteLine("Enter side B: ");
+            int sideB = int.Parse(Console.ReadLine());
+            sideLenghts.Add(sideB);
+
+            Console.WriteLine("Enter side C: ");
+            int sideC = int.Parse(Console.ReadLine());
+            sideLenghts.Add(sideC);
+
+            sideLenghts.Sort();
+
+            int longestSide = (sideLenghts.Max());
+            int sumOfShorterSides = sideLenghts[0] + sideLenghts[1];
+
+            if (sumOfShorterSides > longestSide)
+            {
+                Console.WriteLine("A triangle can be build");
+            }
+            else
+            {
+                Console.WriteLine("A triangle can't be build");
+            }
+        }
+    }
+}
